4a510820d092d045f40b779867765e4347371313
T Sai Ruthvik <f2013489@goa.bits-pilani.ac.in>
2017-10-15 22:19:41 +0530

    lightning in between hands

---
+++ Assets/cubeleap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeleap : MonoBehaviour {
+
+	private Vector3 originalPosition;
+	float xAngle;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		originalPosition = transform.localPosition;
+		xAngle = transform.eulerAngles.x;
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = originalPosition;
+		transform.eulerAngles = new Vector3(xAngle, 0, transform.eulerAngles.z);
+	}
+	void OnCollisionExit(Collision col) {
+		rb.AddForce (col.transform.position);
+	}
+}
